3abf3c989c251e19b562ddc1bde91a4213f956ed
23031702 <moshapomarope500@gmail.com>
2025-04-04 16:29:21 +0200

    Java code

---
+++ src/main/java/org/example/BurmaShave.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class BurmaShave {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"Speeding fast");
+        JOptionPane.showMessageDialog(null,"Might not last");
+        JOptionPane.showMessageDialog(null,"Slow it down");
+        JOptionPane.showMessageDialog(null,"Avoid the crash");
+        JOptionPane.showMessageDialog(null,"Burma shave");
+    }
+}
